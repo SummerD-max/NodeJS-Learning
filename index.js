7339a6307028afca130e47d96239855c089d6262
吴端己 <wuduanji13404@apexsoft.com.cn>
2026-03-16 11:52:07 +0800

    feat: 笔记的增查，重要性调整接口

--- index.js
+++ index.js
@@ -1,91 +1,49 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-let telRecords = [
+const PORT = process.env.PORT || 3001;
+
+let notes = [
   {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456111111",
+    id: 1,
+    content: "HTML is easy",
+    date: "2023-01-01",
+    important: true,
   },
   {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2023-01-02",
+    important: false,
   },
 ];
 
-app.get("/api/persons", (request, response) => {
-  response.json(telRecords);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
-app.get("/api/info", (request, response) => {
-  response.send(
-    `<div><div>Phone has info for ${telRecords.length} people</div><div>${new Date()}</div></div>`,
-  );
-});
-
-app.get("/api/person/:id", (request, response) => {
-  const id = request.params.id;
-  const person = telRecords.find((record) => record.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).json({ error: "person not found" });
-  }
-});
-
-app.delete("/api/person/:id", (request, response) => {
-  const id = request.params.id;
-  const person = telRecords.find((record) => record.id === id);
-  if (person) {
-    telRecords = telRecords.filter((record) => record.id !== id);
-    response.status(204).end();
-  } else {
-    response.status(404).json({ error: "person not found" });
-  }
-});
-
-app.post("/api/person", (request, response) => {
+app.post("/api/notes", (request, response) => {
   const body = request.body;
-
-  const { name, number } = body;
-
-  if (!name || !number) {
-    return response.status(400).json({ error: "name or number is missing" });
-  }
-
-  if (telRecords.some((record) => record.name === name)) {
-    return response.status(400).json({ error: "name must be unique" });
-  }
-
-  const generateId = () => {
-    const maxId = 1000000;
-    return String(Math.floor(Math.random() * maxId));
-  };
-
-  const person = {
-    id: generateId(),
-    name,
-    number,
-  };
-
-  telRecords.push(person);
-
-  response.status(201).json(person);
+  notes.push({
+    ...body,
+    id: notes.length + 1,
+    important: false,
+    date: new Date().toISOString("YYYY-MM-DD"),
+  });
+  response.json(body);
 });
 
-app.listen(3001, () => {
-  console.log("Server running on port 3001");
+app.put("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = request.body;
+  notes = notes.map((n) => (n.id === id ? { ...n, ...note } : n));
+  response.json(note);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
